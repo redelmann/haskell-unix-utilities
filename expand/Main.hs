0ffcc97f0460f72ee6e7f1500d6277de1ca01149
Romain Edelmann <romain.edelmann@gmail.com>
2013-01-03 11:49:11 +0100

    Initial import, added "expand".

---
+++ expand/Main.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE PatternGuards #-}
+
+module Main(main) where
+
+import Prelude hiding (catch)
+import Options.Applicative
+import Control.Error (assertErr, readErr, readMay)
+import Control.Monad.Error.Class (Error, strMsg)
+import Data.List.Split (splitOn)
+import Data.Monoid
+import System.IO
+import System.IO.Error hiding (catch)
+import Control.Exception (catch)
+import System.Exit (exitFailure)
+
+data Options = Options {
+    tabs     :: [Int],
+    encoding :: String,
+    files    :: [String]
+}
+
+readTabs :: Error e => String -> Either e [Int]
+readTabs s = do
+    ts <- mapM (readErr $ strMsg "Supplied argument is not an integer") $ 
+        splitOn "," s
+    assertErr (strMsg "Supplied list of tabs not in order") $ sorted ts
+    return $ listToTabs ts
+
+options :: Parser Options
+options = Options <$> nullOption (short 't'
+                    <> long "tabs" 
+                    <> reader readTabs 
+                    <> metavar "tablist" 
+                    <> help "Specify the tab stops" 
+                    <> value (listToTabs []))
+                  <*> strOption (short 'e'
+                    <> long "enc"
+                    <> metavar "encoding"
+                    <> help "Specify the encoding"
+                    <> value (show localeEncoding))
+                  <*> arguments str (metavar "files...")
+
+listToTabs :: [Int] -> [Int]
+listToTabs [] = listToTabs [8]
+listToTabs [x] = map (*x) [1..] 
+listToTabs xs = xs ++ map (+ last xs) [1..]
+
+sorted :: Ord a => [a] -> Bool
+sorted (x:y:ys) = x < y && sorted (y:ys) 
+sorted _ = True
+
+expandLine :: [Int] -> String -> String
+expandLine ts line = go 0 ts line
+    where
+        go :: Int -> [Int] -> String -> String
+        go p css s = let (c:cs) = dropWhile (<=p) css in case s of
+            ""        -> ""
+            ('\t':xs) -> replicate (c - p) ' ' ++ go c cs xs
+            (x:xs)    -> x : go (p+1) (css) xs
+
+mainWithOptions :: Options -> IO ()
+mainWithOptions opts = (do
+    enc <- mkTextEncoding (encoding opts)
+    hSetEncoding stdin enc
+    if null fs then expandStdIn enc else expandFiles enc) `catch` 
+        (\e -> errorHandler e >> exitFailure)
+    where
+        fs :: [String]
+        fs = files opts 
+
+        ts :: [Int]
+        ts = tabs opts
+
+        expandStdIn :: TextEncoding -> IO ()
+        expandStdIn enc = expandHandle enc stdin
+
+        expandFiles :: TextEncoding -> IO ()
+        expandFiles enc = mapM (expandFile enc) fs >> return ()
+
+        expandFile :: TextEncoding -> String -> IO ()
+        expandFile enc f = withFile f ReadMode (expandHandle enc)
+        
+        expandHandle :: TextEncoding -> Handle -> IO ()
+        expandHandle enc h = (hSetEncoding h enc >> go h) `catch` errorHandler
+            where
+            go :: Handle -> IO ()
+            go h = do
+                eof <- hIsEOF h
+                case eof of
+                    True  -> return ()
+                    False -> do
+                        line <- hGetLine h
+                        putStrLn $ expandLine ts line
+                        go h
+
+        errorHandler :: IOError -> IO ()
+        errorHandler e
+            | isDoesNotExistError e, Just f <- ioeGetFileName e = 
+                hPutStrLn stderr $ "The file " ++ f ++ " does not exist."
+            | isPermissionError e, Just f <- ioeGetFileName e = 
+                hPutStrLn stderr $ "Not enough permission to read file " ++ f ++ "."
+            | otherwise = hPutStrLn stderr "Can not decode with specified encoding."
+
+main :: IO ()
+main = execParser (info (options <**> helper) (fullDesc)) >>= mainWithOptions
